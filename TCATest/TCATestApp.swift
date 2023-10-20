a1771aa36c5c011ee4c1864416fcbdb625d60d48
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-20 12:15:32 +0900

    xcodeの初期ファイル追加

---
+++ TCATest/TCATestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATestApp.swift
+//  TCATest
+//
+//  Created by 柿崎逸 on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TCATestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
